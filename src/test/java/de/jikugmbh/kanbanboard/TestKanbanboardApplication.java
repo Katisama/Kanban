d963a8db6429352c2ce9c8677cb93fac513444c3
Katisama <katarzyna.korczynska2407@gmail.com>
2023-07-06 16:57:10 +0200

    gradle Projekt Spring Boot Grundgerüst initialisiert

---
+++ src/test/java/de/jikugmbh/kanbanboard/TestKanbanboardApplication.java
@@ -0,0 +1,13 @@
+package de.jikugmbh.kanbanboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestKanbanboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(KanbanboardApplication::main).with(TestKanbanboardApplication.class).run(args);
+	}
+
+}
